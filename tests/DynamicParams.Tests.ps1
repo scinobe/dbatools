e68d91ae684b0f0028cb148839dd53caf595fafd
SQLDBAWithABeard <mrrobsewell@outlook.com>
2016-12-04 15:23:47 +0000

    Added Dynamic.Params.Tessts file ALL COMMENTED OUT

---
+++ tests/DynamicParams.Tests.ps1
@@ -0,0 +1,176 @@
+<#
+
+Commented out someone needs to look at 
+
+
+
+
+
+
+
+RuleName                            Severity     FileName   Line  Message                                                     
+--------                            --------     --------   ----  -------                                                     
+PSUseDeclaredVarsMoreThanAssigments Warning      DynamicPar 678   The variable 'usernedponit' is assigned but never used.     
+                                                 ams.ps1                                                                      
+PSPossibleIncorrectComparisonWithNu Warning      DynamicPar 773   $null should be on the left side of equality comparisons.   
+ll                                               ams.ps1                                                                      
+PSPossibleIncorrectComparisonWithNu Warning      DynamicPar 863   $null should be on the left side of equality comparisons.   
+ll                                               ams.ps1                                                                      
+PSPossibleIncorrectComparisonWithNu Warning      DynamicPar 1008  $null should be on the left side of equality comparisons.   
+ll                                               ams.ps1                                                                      
+PSPossibleIncorrectComparisonWithNu Warning      DynamicPar 1021  $null should be on the left side of equality comparisons.   
+ll                                               ams.ps1                                                                      
+PSPossibleIncorrectComparisonWithNu Warning      DynamicPar 1031  $null should be on the left side of equality comparisons.   
+ll                                               ams.ps1                                                                      
+PSPossibleIncorrectComparisonWithNu Warning      DynamicPar 1224  $null should be on the left side of equality comparisons.   
+ll                                               ams.ps1                                                                      
+PSUseSingularNouns                  Warning      DynamicPar 9     The cmdlet 'Get-ParamSqlServerConfigs' uses a plural noun.  
+                                                 ams.ps1          A singular noun should be used instead.                     
+PSUseSingularNouns                  Warning      DynamicPar 56    The cmdlet 'Get-ParamSqlDatabases' uses a plural noun. A    
+                                                 ams.ps1          singular noun should be used instead.                       
+PSUseSingularNouns                  Warning      DynamicPar 203   The cmdlet 'Get-ParamSqlLogins' uses a plural noun. A       
+                                                 ams.ps1          singular noun should be used instead.                       
+PSUseSingularNouns                  Warning      DynamicPar 300   The cmdlet 'Get-ParamSqlServerRoles' uses a plural noun. A  
+                                                 ams.ps1          singular noun should be used instead.                       
+PSUseSingularNouns                  Warning      DynamicPar 348   The cmdlet 'Get-ParamSqlProcessHosts' uses a plural noun. A 
+                                                 ams.ps1          singular noun should be used instead.                       
+PSUseSingularNouns                  Warning      DynamicPar 390   The cmdlet 'Get-ParamSqlProcessPrograms' uses a plural      
+                                                 ams.ps1          noun. A singular noun should be used instead.               
+PSUseSingularNouns                  Warning      DynamicPar 432   The cmdlet 'Get-ParamSqlSpids' uses a plural noun. A        
+                                                 ams.ps1          singular noun should be used instead.                       
+PSUseSingularNouns                  Warning      DynamicPar 475   The cmdlet 'Get-ParamSqlCredentials' uses a plural noun. A  
+                                                 ams.ps1          singular noun should be used instead.                       
+PSUseSingularNouns                  Warning      DynamicPar 521   The cmdlet 'Get-ParamSqlServerAudits' uses a plural noun. A 
+                                                 ams.ps1          singular noun should be used instead.                       
+PSUseSingularNouns                  Warning      DynamicPar 567   The cmdlet 'Get-ParamSqlServerServerAuditSpecifications'    
+                                                 ams.ps1          uses a plural noun. A singular noun should be used instead. 
+PSUseSingularNouns                  Warning      DynamicPar 613   The cmdlet 'Get-ParamSqlBackupDevices' uses a plural noun.  
+                                                 ams.ps1          A singular noun should be used instead.                     
+PSUseSingularNouns                  Warning      DynamicPar 658   The cmdlet 'Get-ParamSqlServerEndpoints' uses a plural      
+                                                 ams.ps1          noun. A singular noun should be used instead.               
+PSUseSingularNouns                  Warning      DynamicPar 705   The cmdlet 'Get-ParamSqlLinkedServers' uses a plural noun.  
+                                                 ams.ps1          A singular noun should be used instead.                     
+PSUseSingularNouns                  Warning      DynamicPar 848   The cmdlet 'Get-ParamSqlExtendedEvents' uses a plural noun. 
+                                                 ams.ps1          A singular noun should be used instead.                     
+PSUseSingularNouns                  Warning      DynamicPar 991   The cmdlet 'Get-ParamSqlCmsGroups' uses a plural noun. A    
+                                                 ams.ps1          singular noun should be used instead.                       
+PSUseSingularNouns                  Warning      DynamicPar 1048  The cmdlet 'Get-ParamSqlServerTriggers' uses a plural noun. 
+                                                 ams.ps1          A singular noun should be used instead.                     
+PSUseSingularNouns                  Warning      DynamicPar 1095  The cmdlet 'Get-ParamSqlCustomErrors' uses a plural noun. A 
+                                                 ams.ps1          singular noun should be used instead.                       
+PSUseSingularNouns                  Warning      DynamicPar 1142  The cmdlet 'Get-ParamSqlDatabaseAssemblies' uses a plural   
+                                                 ams.ps1          noun. A singular noun should be used instead.               
+PSUseSingularNouns                  Warning      DynamicPar 1206  The cmdlet 'Get-ParamSqlDataCollectionSets' uses a plural   
+                                                 ams.ps1          noun. A singular noun should be used instead.               
+PSUseSingularNouns                  Warning      DynamicPar 1264  The cmdlet 'Get-ParamSqlAlerts' uses a plural noun. A       
+                                                 ams.ps1          singular noun should be used instead.                       
+PSUseSingularNouns                  Warning      DynamicPar 1307  The cmdlet 'Get-ParamSqlOperators' uses a plural noun. A    
+                                                 ams.ps1          singular noun should be used instead.                       
+PSUseSingularNouns                  Warning      DynamicPar 1349  The cmdlet 'Get-ParamSqlOperatorCategories' uses a plural   
+                                                 ams.ps1          noun. A singular noun should be used instead.               
+PSUseSingularNouns                  Warning      DynamicPar 1392  The cmdlet 'Get-ParamSqlProxyAccounts' uses a plural noun.  
+                                                 ams.ps1          A singular noun should be used instead.                     
+PSUseSingularNouns                  Warning      DynamicPar 1436  The cmdlet 'Get-ParamSqlSharedSchedules' uses a plural      
+                                                 ams.ps1          noun. A singular noun should be used instead.               
+PSUseSingularNouns                  Warning      DynamicPar 1478  The cmdlet 'Get-ParamSqlJobCategories' uses a plural noun.  
+                                                 ams.ps1          A singular noun should be used instead.                     
+PSUseSingularNouns                  Warning      DynamicPar 1520  The cmdlet 'Get-ParamSqlAlertCategories' uses a plural      
+                                                 ams.ps1          noun. A singular noun should be used instead.               
+PSUseSingularNouns                  Warning      DynamicPar 1562  The cmdlet 'Get-ParamSqlTargetServers' uses a plural noun.  
+                                                 ams.ps1          A singular noun should be used instead.                     
+PSUseSingularNouns                  Warning      DynamicPar 1604  The cmdlet 'Get-ParamSqlTargetServerGroups' uses a plural   
+                                                 ams.ps1          noun. A singular noun should be used instead.               
+PSUseSingularNouns                  Warning      DynamicPar 1646  The cmdlet 'Get-ParamSqlJobs' uses a plural noun. A         
+                                                 ams.ps1          singular noun should be used instead.                       
+PSUseSingularNouns                  Warning      DynamicPar 1690  The cmdlet 'Get-ParamSqlAgentCategories' uses a plural      
+                                                 ams.ps1          noun. A singular noun should be used instead.               
+PSUseSingularNouns                  Warning      DynamicPar 1732  The cmdlet 'Get-ParamSqlDatabaseFileTypes' uses a plural    
+                                                 ams.ps1          noun. A singular noun should be used instead.               
+PSUseSingularNouns                  Warning      DynamicPar 1847  The cmdlet 'Get-ParamSqlMaintenancePlans' uses a plural     
+                                                 ams.ps1          noun. A singular noun should be used instead.               
+PSUseSingularNouns                  Warning      DynamicPar 1892  The cmdlet 'Get-ParamSqlAvailabilityGroups' uses a plural   
+                                                 ams.ps1          noun. A singular noun should be used instead.               
+PSAvoidUsingEmptyCatchBlock         Warning      DynamicPar 1177  Empty catch block is used. Please use Write-Error or throw  
+                                                 ams.ps1          statements in catch blocks.                                 
+
+
+
+
+
+
+#Thank you Warren http://ramblingcookiemonster.github.io/Testing-DSC-with-Pester-and-AppVeyor/
+
+if(-not $PSScriptRoot)
+{
+    $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent
+}
+$Verbose = @{}
+if($env:APPVEYOR_REPO_BRANCH -and $env:APPVEYOR_REPO_BRANCH -notlike "master")
+{
+    $Verbose.add("Verbose",$True)
+}
+
+
+
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace('.Tests.', '.')
+Import-Module $PSScriptRoot\..\functions\$sut -Force
+Import-Module PSScriptAnalyzer
+## Added PSAvoidUsingPlainTextForPassword as credential is an object and therefore fails. 
+## We can ignore any rules here under special circumstances agreed by admins :-)
+## We expect some context using comments about the reason for ignoring a rule
+
+$Rules = (Get-ScriptAnalyzerRule).Where{$_.RuleName -notin ('PSAvoidUsingPlainTextForPassword') }
+$Name = $sut.Split('.')[0]
+
+    Describe 'Script Analyzer Tests' {
+            Context "Testing $Name for Standard Processing" {
+                foreach ($rule in $rules) { 
+                    $i = $rules.IndexOf($rule)
+                    It "passes the PSScriptAnalyzer Rule number $i - $rule  " {
+                        (Invoke-ScriptAnalyzer -Path "$PSScriptRoot\..\functions\$sut" -IncludeRule $rule.RuleName ).Count | Should Be 0 
+                    }
+                }
+            }
+        }
+   ## Load the command
+$ModuleBase = Split-Path -Parent $MyInvocation.MyCommand.Path
+
+# For tests in .\Tests subdirectory
+if ((Split-Path $ModuleBase -Leaf) -eq 'Tests')
+{
+	$ModuleBase = Split-Path $ModuleBase -Parent
+}
+
+# Handles modules in version directories
+$leaf = Split-Path $ModuleBase -Leaf
+$parent = Split-Path $ModuleBase -Parent
+$parsedVersion = $null
+if ([System.Version]::TryParse($leaf, [ref]$parsedVersion))
+{
+	$ModuleName = Split-Path $parent -Leaf
+}
+else
+{
+	$ModuleName = $leaf
+}
+
+# Removes all versions of the module from the session before importing
+Get-Module $ModuleName | Remove-Module
+
+# Because ModuleBase includes version number, this imports the required version
+# of the module
+$null = Import-Module $ModuleBase\$ModuleName.psd1 -PassThru -ErrorAction Stop 
+. "$Modulebase\functions\DynamicParams.ps1"
+Get-ChildItem "$Modulebase\internal\" |% {. $_.fullname}
+
+    Describe "$Name Tests"{
+        InModuleScope 'dbatools' {
+            Context " There should be some functional tests here" {
+                It "Does a thing" {
+                    $ActualValue | Should Be $ExpectedValue
+                }
+		    }# Context
+        }#modulescope
+    }#describe
+    #>
+    
