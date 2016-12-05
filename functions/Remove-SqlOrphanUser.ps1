dc45c5f767a873c49fbd9be901ff3ef0e8cdd619
SQLDBAWithABeard <mrrobsewell@outlook.com>
2016-12-05 10:38:01 +0000

    removed aliases

--- functions/Remove-SqlOrphanUser.ps1
+++ functions/Remove-SqlOrphanUser.ps1
@@ -160,7 +160,7 @@ Will remove from all databases the user OrphanUser EVEN if exists their matching
 
                         if ($Users.Count -eq 0)
                         {
-                            $Users = $db.Users | Where {$_.Login -eq "" -and ("dbo","guest","sys","INFORMATION_SCHEMA" -notcontains $_.Name)}
+                            $Users = $db.Users | Where-Object {$_.Login -eq "" -and ("dbo","guest","sys","INFORMATION_SCHEMA" -notcontains $_.Name)}
                         }
                         else
                         {
@@ -171,7 +171,7 @@ Will remove from all databases the user OrphanUser EVEN if exists their matching
 		                    }
                             else
                             {
-                                $Users = $db.Users | Where {$_.Login -eq "" -and ($Users -contains $_.Name)}
+                                $Users = $db.Users | Where-Object {$_.Login -eq "" -and ($Users -contains $_.Name)}
                             }
                         }
                     }
